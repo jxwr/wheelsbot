a991f84fc2d42cd7f6e4713dd0c7df4ddabadea5
jxwr <jxwr.cn@gmail.com>
2026-01-18 23:35:02 +0800

    open loop

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,18 @@ static constexpr int   POLE_PAIRS      = 7;
 static constexpr float SUPPLY_VOLTAGE  = 12.0f;
 static constexpr float VOLTAGE_LIMIT   = 3.0f;   // 先小一点，确认方向/闭环稳定再加
 
+void deadloop() {
+  while (true) {
+    vTaskDelay(pdMS_TO_TICKS(500));
+    Serial.println("reach here!");
+  }
+}
+
 // ====== SimpleFOC objects ======
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 BLDCMotor motor(POLE_PAIRS);
 BLDCDriver3PWM driver(PIN_PWM_U, PIN_PWM_V, PIN_PWM_W, PIN_EN);
+Commander command = Commander(Serial);
 
 // 控制目标（可以被别的 task 改，比如 HTTP task）
 volatile float target_velocity = 5.0f; // rad/s
@@ -31,23 +39,17 @@ void ledTask(void* arg) {
   pinMode(LED_PIN, OUTPUT);
   while (true) {
     digitalWrite(LED_PIN, HIGH);
-    vTaskDelay(pdMS_TO_TICKS(500));
+    vTaskDelay(pdMS_TO_TICKS(3000));
     digitalWrite(LED_PIN, LOW);
-    vTaskDelay(pdMS_TO_TICKS(500));
+    vTaskDelay(pdMS_TO_TICKS(3000));
 
     Serial.println("LED: blink!");
   }
 }
 
-void deadloop() {
-  while (true) {
-    vTaskDelay(pdMS_TO_TICKS(500));
-    Serial.println("reach here!");
-  }
-}
 
 // ---------------- FOC task ----------------
-void focTask(void* arg) {
+void focSensorTestTask(void* arg) {
   // I2C + sensor init
   Wire.begin((int)PIN_I2C_SDA, (int)PIN_I2C_SCL);
   Wire.setClock(100000);
@@ -100,9 +102,67 @@ void focTask(void* arg) {
   }
 }
 
+void doTarget(char* cmd) { command.scalar(&motor.target, cmd); }
+void doLimit(char* cmd) { command.scalar(&motor.voltage_limit, cmd); }
+
+void focDriverTestTask(void* arg) {
+  driver.voltage_power_supply = 12;
+  // Max DC voltage allowed - default voltage_power_supply
+  driver.voltage_limit = 6;
+
+    // driver init
+  if (!driver.init()){
+    Serial.println("Driver init failed!");
+    return;
+  }
+
+   // enable driver
+  driver.enable();
+
+  motor.linkDriver(&driver);
+
+  // limiting motor movements
+  // limit the voltage to be set to the motor
+  // start very low for high resistance motors
+  // current = voltage / resistance, so try to be well under 1Amp
+  motor.voltage_limit = 10;   // [V]
+ 
+  // open loop control config
+  motor.controller = MotionControlType::velocity_openloop;
+
+  // init motor hardware
+  motor.init();
+
+  // set the target velocity [rad/s]
+  motor.target = 20; // one rotation per second
+
+  motor.enable();  
+
+  // add target command T
+  command.add('T', doTarget, "target velocity");
+  command.add('L', doLimit, "voltage limit");
+
+  Serial.println("Motor ready!");
+  Serial.println("Set target velocity [rad/s]");
+  Serial.println("Driver ready!");
+
+  _delay(5000);
+  
+  while (true) {
+    // open loop velocity movement
+    motor.move();
+    vTaskDelay(pdMS_TO_TICKS(1));  // 让出 CPU
+
+    // user communication
+    command.run();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   delay(500);
+
+  SimpleFOCDebug::enable(&Serial);
 
   // LED 任务：低优先级即可
   xTaskCreatePinnedToCore(
@@ -116,14 +176,26 @@ void setup() {
   );
 
   // FOC 任务：更高优先级，尽量不被别的任务打断
+  /*
   xTaskCreatePinnedToCore(
-    focTask,
-    "FOC",
+    focSensorTestTask,
+    "FOC_Sensor",
     8192,
     nullptr,
     3,
     nullptr,
     1
+  );
+  */
+
+  xTaskCreatePinnedToCore(
+     focDriverTestTask,
+     "FOC_Driver",
+     8192,
+     nullptr,
+     3,
+     nullptr,
+     1
   );
 
   Serial.println("Tasks started");
